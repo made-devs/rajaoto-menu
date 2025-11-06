5677c36315909d37c5d8ed163c8a9069f6bf3d56
madedev <nmaderama@gmail.com>
2025-11-06 23:27:45 +0700

    Fix layout max width

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,21 @@
 // app/layout.jsx
-import "./globals.css";
-import { Montserrat } from "next/font/google"; // Import font
-import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
-import { GsapInitializer } from "./components/GsapInitializer";
-import { Footer } from "./components/Footer";
+import './globals.css';
+import { Montserrat } from 'next/font/google'; // Import font
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { GsapInitializer } from './components/GsapInitializer';
+import { Footer } from './components/Footer';
 
 // Konfigurasi font
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-montserrat", // Buat variabel CSS jika diperlukan
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat', // Buat variabel CSS jika diperlukan
 });
 
 export const metadata = {
-  title: "TJM Auto Care",
-  description: "Solusi terbaik untuk semua keresahan mobil Anda",
+  title: 'TJM Auto Care',
+  description: 'Solusi terbaik untuk semua keresahan mobil Anda',
 };
 
 export default function RootLayout({ children }) {
@@ -30,13 +30,13 @@ export default function RootLayout({ children }) {
           <div id="smooth-content">
             {/* Lebar container diubah agar responsif di tablet dan desktop */}
             <div
-              className="mx-auto max-w-md md:max-w-4xl text-white shadow-2xl min-h-screen flex flex-col"
+              className="mx-auto max-w-md md:max-w-3xl text-white shadow-2xl min-h-screen flex flex-col"
               style={{
                 backgroundImage:
                   "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.5)), url('/bg.webp')",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
               }}
             >
               <Header />
